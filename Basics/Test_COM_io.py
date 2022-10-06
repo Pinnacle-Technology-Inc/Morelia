8dd3e45cf33282216b10b03ffdf8f86f19ef8360
ThresaKelly <tkelly@pinnaclet.com>
2022-10-06 16:52:41 -0500

    [PA-1] cleanup and rename
    COM_io is done think!

--- Basics/Test_COM_io.py
+++ Basics/Test_COM_io.py
@@ -1,4 +1,4 @@
-from COMport_InOut import COM_io
+from Serial_InOut import COM_io
 
 # get port list 
 portList = COM_io.GetCOMportsList()
@@ -27,9 +27,15 @@ else:
 com = COM_io(portUse)
 print('serial port in use:', com.GetPortName())
 
-# read!
-while True:
-    # packet = com.ReadLine()
-    # packet = com.Read(7)
-    packet = com.ReadUntil(b'\r')
-    print(packet)
+# # read!
+# while True:
+#     # packet = com.ReadLine()
+#     # packet = com.Read(7)
+#     packet = com.ReadUntil(b'\r')
+#     print(packet)
+
+ping = COM_io.HexStrToByteArray('0230303032334403')
+stopat = COM_io.HexStrToByteArray('03')
+
+com.Write(ping)
+print('PING:', com.ReadUntil(stopat))
