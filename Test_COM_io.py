a142807df7f26e2969d05a861250ad810ff2f8d0
ThresaKelly <tkelly@pinnaclet.com>
2022-10-13 16:29:12 -0500

    [PA-2] cleanup old code

--- Test_COM_io.py
+++ Test_COM_io.py
@@ -43,50 +43,6 @@ from BasicPodProtocol import POD_Basics
 # com.Write(ping)
 # print('PING:', com.ReadUntil(stopat))
 
-# myarr = '010203'
-# bytearr = bytearray.fromhex(myarr)
-# print(bytearr)
-
-# b0 = 0x01
-# b1 = 0x02
-# b2 = 0x03
-# blist = [b0,b1,b2]
-# bytearr = bytearray(blist)
-# print(bytearr)
-# print(bytearr[0])
-# print(bytearr[1])
-# print(bytearr[2])
-
-# blist = [0x30,0x30,0x30,0x36,0x30,0x01]
-blist = [0x30,0x30,0x30,0x32]
-barr = bytearray(blist)
-
-sum = 0
-for b in barr: 
-    sum = sum + b
-out = ~sum & 0xFF # get an unsigned 8 bit number range (0..255)
-outstr = hex(out)
-
-# get last to characters and make uppercase
-c0 = outstr[len(outstr)-2].upper()
-c1 = outstr[len(outstr)-1].upper()
-c = [c0,c1]
-print(c)
-
-# get ascii code 
-ascii0 = ord(c0)
-ascii1 = ord(c1)
-asc = [ascii0,ascii1]
-print(ascii0)
-print(ascii1)
-
-print(hex(ascii0))
-print(hex(ascii1))
-
-arr = bytes(asc)
-print(arr)
-print(hex(arr[0]))
-print(hex(arr[1]))
-
+barr = bytearray([0x30,0x30,0x30,0x32])
 check = POD_Basics.ChecksumBytes(barr)
 print(check)
