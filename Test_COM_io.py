69db232bf1af83189a0b615b940222298f82e991
ThresaKelly <tkelly@pinnaclet.com>
2022-10-20 15:45:30 -0500

    [PA-2] cleanup

--- Test_COM_io.py
+++ Test_COM_io.py
@@ -1,33 +1,32 @@
-from tabnanny import check
 from Serial_InOut import COM_io
 from BasicPodProtocol import POD_Basics
 
-# # get port list 
-# portList = COM_io.GetCOMportsList()
+# get port list 
+portList = COM_io.GetCOMportsList()
 
-# # check if the list is empty 
-# if (len(portList) == 0):
-#     raise Exception('[!] No COM ports in use.')
-# # use single option 
-# elif (len(portList) == 1): 
-#     phrase = portList[0].split(' ')
-#     portUse = phrase[0]
-# # choose port 
-# else:
-#     # request port from user
-#     comNum = input('Select port: COM')
-#     # search for port
-#     portUse = None
-#     for port in portList:
-#         if port.startswith('COM'+comNum):
-#             portUse = 'COM' + comNum 
-#     # check if port exists
-#     if portUse==None : 
-#         raise Exception('[!] COM port does not exist.')
+# check if the list is empty 
+if (len(portList) == 0):
+    raise Exception('[!] No COM ports in use.')
+# use single option 
+elif (len(portList) == 1): 
+    phrase = portList[0].split(' ')
+    portUse = phrase[0]
+# choose port 
+else:
+    # request port from user
+    comNum = input('Select port: COM')
+    # search for port
+    portUse = None
+    for port in portList:
+        if port.startswith('COM'+comNum):
+            portUse = 'COM' + comNum 
+    # check if port exists
+    if portUse==None : 
+        raise Exception('[!] COM port does not exist.')
 
-# # create COM object 
-# com = COM_io(portUse)
-# print('serial port in use:', com.GetPortName())
+# create COM object 
+com = COM_io(portUse)
+print('serial port in use:', com.GetPortName())
 
 # # === test functions of COM_io
 
@@ -48,17 +47,3 @@ from BasicPodProtocol import POD_Basics
 # check = POD_Basics.ChecksumBytes(barr)
 # print(check)
 
-
-stx = bytes.fromhex('02')
-cmd = POD_Basics.ValueToBytes(2, 4) 
-cs = POD_Basics.Checksum(cmd)
-etx = bytes.fromhex('03')
-message = stx + cmd + cs + etx
-print(message)
-
-# next fix checksum to handle valueto bytes 
-
-# # test
-# msg = POD_Basics.ValueToBytes(2, 4) 
-# for b in msg:
-#     print(hex(b))
