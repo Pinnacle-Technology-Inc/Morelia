fa3816af6e3aeda099d78687b5e827ae02901827
ThresaKelly <tkelly@pinnaclet.com>
2022-10-27 14:29:21 -0500

    [PA-2] add command dicts and update functions

--- BasicPodProtocol.py
+++ BasicPodProtocol.py
@@ -4,9 +4,30 @@ class POD_Basics(COM_io) :
 
     # ====== GLOBAL VARIABLES ======
 
-    __standard_commandNumbers  = [0,1,2,3,4,5,7,8,9,10,12]
-    __payload_commandNumbers   = [6] #,11] # 11 is binary... figure this one out later
-    __payload_argumentBytes    = [2] #,??]
+    # index keys for dict values 
+    __NAME = 0
+    __RETURNS = 1
+    __ARGUMRNTS = 2
+
+    __standard_commands = {
+        # key(command number) : value([command name, return bytes]) 
+        0   : [ 'ACK',                  0       ],
+        1   : [ 'NACK',                 0       ],
+        2   : [ 'PING',                 0       ],
+        3   : [ 'RESET',                0       ],
+        4   : [ 'ERROR',                1       ],
+        5   : [ 'STATUS',               None    ],  # NONE is placeholder, I dont know this now
+        7   : [ 'BOOT',                 None    ],
+        8   : [ 'TYPE',                 1       ],
+        9   : [ 'ID',                   None    ],
+        10  : [ 'SAMPLE RATE',          None    ],
+        12  : [ 'FIRMWARE VERSION',     3       ]
+    }
+
+    __payload_commands = {
+        # key(command number) : value([command name, number of argument bytes, number of return bytes]) 
+        6   : [ 'STREAM',               1,      1   ] 
+    }
 
     # ====== STATIC METHODS ======
 
@@ -122,36 +143,105 @@ class POD_Basics(COM_io) :
     # ------ GETTERS ------
 
     def GetStandardCommandNumbers(self):
-        return(self.__standard_commandNumbers)
+        return(self.__standard_commands)
     
     def GetPayloadCommandNumbers(self):
-        return(self.__payload_commandNumbers)
-    
-    def GetPayloadArgumentBytes(self):
-        self.__payload_argumentBytes
+        return(self.__payload_commands)
 
     # ------ SETTERS ------
 
-    def SetStandardCommands(self, cmdList) : 
-            self.__standard_commandNumbers = cmdList
+    def SetStandardCommands(self, cmdDict) : 
+            self.__standard_commands = cmdDict
 
-    def SetPayloadCommands(self, cmdList, argList) : 
-            self.__payload_commandNumbers = cmdList
-            self.__payload_argumentBytes = argList
+    def SetPayloadCommands(self, cmdDict) : 
+            self.__payload_commands = cmdDict
 
     # ------ POD FUNCTIONS ------
+
+    def GetCommandNumber_Standard(self, name) : 
+        # search through dict to find key 
+        for key,val in self.__standard_commands.items() :
+            if(name == val) : 
+                return(key)
+        # no match
+        return(None)
+
+    def GetCommandNumber_Payload(self, name) : 
+        # search through dict to find key 
+        for key,val in self.__payload_commands.items() :
+            if(name == val[self.NAME]) : 
+                # return the command number 
+                return(key)
+        # no match
+        return(None) 
+
+    def GetArgumentBytes_Payload(self, cmd) :
+        # search through dict to find matching entry  
+        for key,val in self.__payload_commands.items() :
+            if(cmd == key or cmd == val[self.NAME]) : 
+                # return the number of bytes in the command return 
+                return(val[self.ARGUMRNTS])
+        # no match
+        return(None) 
+
+    def WriteStandardPacket(self, cmd) : 
+        # throw exception if command number is invalid 
+        if(cmd not in self.__standard_commands) : 
+            raise Exception('Invalid POD command.')
+
+        # get command number 
+        if(isinstance(cmd,str)):
+            cmdNum = self.GetCommandNumber_Standard(cmd)
+        else: 
+            cmdNum = cmd
+
+        # build packet
+        packet = POD_Basics.PODpacket_standard(cmdNum)
+
+        # write packet to serial port 
+        self.Write(packet)
+
+    def WritePayloadPacket(self, cmd, payload) : 
+        # throw exception if command number is invalid 
+        isValidCmd = False
+        for key,val in self.__payload_commandNumbers.items() : 
+            if(cmd==key or cmd==val[self.NAME]):
+                isValidCmd = True
+        if(not isValidCmd) :
+            raise Exception('Invalid POD command number.')
+
+        # throw exception if payload is wrong size 
+        payloadSize = self.GetArgumentBytes_Payload(cmd)
+        if(len(payload) != payloadSize):
+            raise Exception('POD packet payload is invalid.')
+
+        # get command number 
+        if(isinstance(cmd,str)):
+            cmdNum = self.GetCommandNumber_Payload(cmd)
+        else: 
+            cmdNum = cmd
+
+        # build packet
+        packet = POD_Basics.PODpacket_payload(cmdNum, payload)
+
+        # write packet to serial port 
+        self.Write(packet)
+
 
     def ReadPodPacket(self) :      
         # init
         time = 0
         TIMEOUT = 100   
         b = None 
+
         # read until STX found
         while(b != self.STX() and time<TIMEOUT) :
             time += 1           # increment counter
             b = self.Read(1)    # read next byte  
+
         # set first byte of packet to STX
         packet = b
+
         # get bytes until ETX, or start over at next STX
         while(b != self.ETX() and time<TIMEOUT) : 
             time += 1           # increment counter
@@ -163,34 +253,14 @@ class POD_Basics(COM_io) :
             else : 
                 # append byte to end message
                 packet = packet + b
+
         # raise exception if timeout occurs
         if(time==TIMEOUT) : 
             raise Exception('Timout when reading from POD device.')
+
         # return packet containing STX+message+ETX
         return(packet)
-        
-    def WriteStandardPacket(self, commandNumber) : 
-        # throw exception if command number is invalid 
-        if(commandNumber not in self.__standard_commandNumbers) : 
-            raise Exception('Invalid POD command number.')
-        # build packet
-        packet = POD_Basics.PODpacket_standard(commandNumber)
-        # write packet to serial port 
-        self.Write(packet)
 
-    def WritePayloadPacket(self, commandNumber, payload) : 
-        # throw exception if command number is invalid 
-        if(commandNumber not in self.__payload_commandNumbers) : 
-            raise Exception('Invalid POD command number.')
-        # throw exception if payload is wrong size 
-        idx = self.__payload_commandNumbers.index(commandNumber) # get index of command number
-        payloadSize = self.__payload_argumentBytes[idx] # get number of bytes for this command
-        if(len(payload) != payloadSize):
-            raise Exception('POD packet payload is invalid.')
-        # build packet
-        packet = POD_Basics.PODpacket_payload(commandNumber, payload)
-        # write packet to serial port 
-        self.Write(packet)
 
 # TODO 
 # 1. change how to handle command numbers and stuff. Instead of a list, use a tuple/dict. Store the command number, name, and argument (if applicable)
