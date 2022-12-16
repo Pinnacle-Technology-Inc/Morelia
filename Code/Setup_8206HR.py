d16dafe61737cbd6ce8be8374b1cf5d32cf54b1e
ThresaKelly <tkelly@pinnaclet.com>
2022-12-16 15:27:41 -0600

    [PA-6] set sample rate

--- Code/Setup_8206HR.py
+++ Code/Setup_8206HR.py
@@ -10,9 +10,9 @@ class Setup_8206HR :
     def Run(self) :
 
         # TODO
-        # - get port from comport list
-        # - create pod device and connect to comport 
-        # - test connection using PING 
+        # DONE - get port from comport list
+        # DONE - create pod device and connect to comport 
+        # DONE - test connection using PING 
         # - setup sample rate, LP1, LP2, LP3, gain
         # - setup file to save to
         # - start streaming
@@ -23,7 +23,7 @@ class Setup_8206HR :
 
 
         # get the number of devices 
-        numDevices = self.SetNumberOfDevices()
+        numDevices = self._SetNumberOfDevices()
 
         # initialize lists 
         portNames = [None] * numDevices
@@ -36,18 +36,18 @@ class Setup_8206HR :
             print('\n- Device #'+str(i+1))
 
             # get name of port
-            portNames[i] = self.ChoosePort(forbidden=portNames)
+            portNames[i] = self._ChoosePort(forbidden=portNames)
             port = portNames[i].split(' ')[0] # isolate 'COM#' from full port name 
 
             # get baud rate 
-            baudrate = self.ChooseBaudrate(default=False)
+            baudrate = self._ChooseBaudrate()
 
-            # setup device 
+            # create POD device 
             try :
                 # create POD device
                 podDevices[i] = POD_8206HR(port=port, baudrate=baudrate)
                 # ping to test connection 
-                self.TestConnection(podDevices[i])
+                self._TestConnection(podDevices[i])
                 print('Successfully connected '+port+' to Device #'+str(i+1)+'.')
             except :
                 # fail message 
@@ -58,30 +58,35 @@ class Setup_8206HR :
                 # retry 
                 continue
 
+            # setup device 
+            self._ChooseSampleRate(podDevices[i])
+
             # move to next device 
             i+=1
         
 
 
-    # ============ PRIVATE METHODS ============      ========================================================================================================================
+    # ============ PROTECTED METHODS ============      ========================================================================================================================
 
-    def SetNumberOfDevices(self) : 
+    @staticmethod
+    def _SetNumberOfDevices() : 
         try : 
             # request user imput
             n = int(input('How many POD devices do you want to use?: '))
             # number must be positive
             if(n<=0):
                 print('[!] Number must be greater than zero.')
-                return(self.SetNumberOfDevices())
+                return(Setup_8206HR._SetNumberOfDevices())
             # return number of POD devices 
             return(n)
         except : 
             # print error and start over
             print('[!] Please enter an integer number.')
-            return(self.SetNumberOfDevices())
+            return(Setup_8206HR._SetNumberOfDevices())
 
 
-    def GetPortsList(self,forbidden=[]) : 
+    @staticmethod
+    def _GetPortsList(forbidden=[]) : 
         # get port list 
         portListAll = COM_io.GetCOMportsList()
         # remove forbidden ports 
@@ -99,9 +104,10 @@ class Setup_8206HR :
         return(portList)
 
 
-    def ChoosePort(self, forbidden=[]) : 
+    @staticmethod
+    def _ChoosePort(forbidden=[]) : 
         # get ports
-        portList = self.GetPortsList(forbidden)
+        portList = Setup_8206HR._GetPortsList(forbidden)
         print('Available COM Ports:', portList)
         # request port from user
         choice = input('Select port: COM')
@@ -111,13 +117,14 @@ class Setup_8206HR :
                 return(port)
         # if return condition not reached...
         print('[!] COM'+choice+' does not exist. Try again.')
-        return(self.ChoosePort(forbidden))
+        return(Setup_8206HR._ChoosePort(forbidden))
 
     
-    def ChooseBaudrate(self, default=False):
+    @staticmethod
+    def _ChooseBaudrate(useDefault=False, defaultValue=9600):
         # return default
-        if(default):
-            return(9600)
+        if(useDefault):
+            return(defaultValue)
         # else use user input 
         try : 
             # request user imput
@@ -125,21 +132,47 @@ class Setup_8206HR :
             # number must be positive
             if(n<=0):
                 print('[!] Number must be greater than zero.')
-                return(self.ChooseBaudrate(default))
+                return(Setup_8206HR._ChooseBaudrate(useDefault,defaultValue))
             # return baudrate
             return(n)
         except : 
             # print error and start over
             print('[!] Please enter an integer number.')
-            return(self.ChooseBaudrate(default))
+            return(Setup_8206HR._ChooseBaudrate(useDefault,defaultValue))
 
 
-    def TestConnection(self, pod):
-        # write ping, excpect to read ping 
-        w = pod.WritePacket('PING')
-        r = pod.ReadPODpacket()
+    @staticmethod
+    def _TestConnection(pod):
         # connection successful if write and read match 
-        if(w==r):
-            return(True)
-        else:
+        if(not pod.WritePacket('PING') == pod.ReadPODpacket()):
             raise Exception('[!] Connection failed. ')
+
+    
+    @staticmethod
+    def _ChooseSampleRate(pod):
+        try : 
+            # get sample rate from user 
+            sampleRate = int(input('Sample rate: '))
+        except : 
+            # if bad input, start over 
+            print('[!] Please enter an integer number.')
+            return(Setup_8206HR._ChooseSampleRate(pod))
+
+        # check for valid input
+        if(sampleRate<100 or sampleRate>2000) : 
+            print('[!] Sample rate must be between 100-2000.')
+            return(Setup_8206HR._ChooseSampleRate(pod))
+
+        # write sample rate to device 
+        w = pod.WritePacket('SET SAMPLE RATE', sampleRate)
+
+
+    @staticmethod
+    def _ChooseLowpassForEEG(pod):
+        # 0 = EEG1, 1 = EEG2, 2 = EEG3/EMG
+        for eeg in range(3) : 
+            Setup_8206HR._ChooseLowpassForEEG(eeg, pod)
+
+    @staticmethod
+    def _ChooseLowpassForEEG(eeg, pod):
+        pass
