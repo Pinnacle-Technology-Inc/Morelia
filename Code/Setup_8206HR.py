e964a5c48dfb3e9b0b9033341c0d3ad9dab24870
ThresaKelly <tkelly@pinnaclet.com>
2022-12-16 10:03:09 -0600

    [PA-6] create setup py file

---
+++ Code/Setup_8206HR.py
@@ -0,0 +1,20 @@
+from PodDevice_8206HR import POD_8206HR
+
+class Setup_8206HR : 
+    
+
+    def Run(self) :
+
+        # TODO
+        # - get port from comport list
+        # - create pod device and connect to comport 
+        # - test connection using PING 
+        # - setup sample rate, LP1, LP2, LP3, gain
+        # - setup file to save to
+        # - start streaming
+        # - continually get data
+        # - make plot using data
+        # - save data to file 
+
+        pass
+    
