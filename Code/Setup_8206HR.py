99be924c68440829b7b5549fcfa8ce812ed3fc98
ThresaKelly <tkelly@pinnaclet.com>
2022-12-16 12:48:26 -0600

    [PA-6] request ports from user

--- Code/Setup_8206HR.py
+++ Code/Setup_8206HR.py
@@ -1,7 +1,11 @@
-from PodDevice_8206HR import POD_8206HR
+import time 
+
+from SerialCommunication    import COM_io
+from PodDevice_8206HR       import POD_8206HR
 
 class Setup_8206HR : 
     
+    # ============ PUBLIC METHODS ============      ========================================================================================================================
 
     def Run(self) :
 
@@ -16,5 +20,63 @@ class Setup_8206HR :
         # - make plot using data
         # - save data to file 
 
-        pass
-    
+
+
+        # get the number of devices 
+        numDevices = self.SetNumberOfDevices()
+        print('\n')
+
+        # get port name of each device 
+        portNames = [''] * numDevices
+        for i in range(numDevices) : 
+            print('- Device #',i+1)
+            portNames[i] = self.ChoosePort(forbidden=portNames)
+            print('\n')
+        print('Selected Ports:', portNames)
+
+        
+
+    # ============ PRIVATE METHODS ============      ========================================================================================================================
+
+    def SetNumberOfDevices(self) : 
+        try : 
+            # request user imput
+            n = int(input('How many POD devices do you want to use?: '))
+            return(n)
+        except : 
+            # print error and start over
+            print('[!] Please enter an integer number.')
+            return(self.SetNumberOfDevices())
+
+
+    def GetPortsList(self,forbidden=[]) : 
+        # get port list 
+        portListAll = COM_io.GetCOMportsList()
+        # remove forbidden ports 
+        portList = [x for x in portListAll if x not in forbidden]
+
+        # check if the list is empty 
+        if (len(portList) == 0):
+            # print error and keep trying to get ports
+            print('[!] No COM ports in use. Please plug in POD device.')
+            while(len(portList) == 0) : 
+                portListAll = COM_io.GetCOMportsList()
+                portList = [x for x in portListAll if x not in forbidden]
+
+        # return port
+        return(portList)
+
+
+    def ChoosePort(self, forbidden=[]) : 
+        # get ports
+        portList = self.GetPortsList(forbidden)
+        print('Available COM Ports:', portList)
+        # request port from user
+        choice = input('Select port: COM')
+        # search for port in list
+        for port in portList:
+            if port.startswith('COM'+choice):
+                return(port)
+        # if return condition not reached...
+        print('[!] COM'+choice+' does not exist. Try again.')
+        return(self.ChoosePort(forbidden))
