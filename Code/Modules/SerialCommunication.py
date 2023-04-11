ad1fcb8e820310234f9d081e32e48728c78b8906
ThresaKelly <tkelly@pinnaclet.com>
2023-04-11 11:24:01 -0500

    [PA-8] add copyright

--- Code/Modules/SerialCommunication.py
+++ Code/Modules/SerialCommunication.py
@@ -10,6 +10,7 @@ __author__      = "Thresa Kelly"
 __maintainer__  = "Thresa Kelly"
 __credits__     = ["Thresa Kelly", "Seth Gabbert"]
 __license__     = "New BSD License"
+__copyright__   = "Copyright (c) 2023, Thresa Kelly"
 __email__       = "sales@pinnaclet.com"
 
 class COM_io : 
