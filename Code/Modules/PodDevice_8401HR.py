e946ed4dbb2057ef76e06f64a4c69e6c12ab86ec
ThresaKelly <tkelly@pinnaclet.com>
2023-04-18 12:39:01 -0500

    [PA-11] class header

---
+++ Code/Modules/PodDevice_8401HR.py
@@ -0,0 +1,29 @@
+"""
+POD_8401HR handles communication using an 8401-HR POD device. 
+"""
+
+# local imports 
+from BasicPodProtocol       import POD_Basics
+from PodPacketHandling      import POD_Packets
+from PodCommands            import POD_Commands
+
+# authorship
+__author__      = "Thresa Kelly"
+__maintainer__  = "Thresa Kelly"
+__credits__     = ["Thresa Kelly", "Seth Gabbert"]
+__license__     = "New BSD License"
+__copyright__   = "Copyright (c) 2023, Thresa Kelly"
+__email__       = "sales@pinnaclet.com"
+
+class POD_8401HR(POD_Basics) : 
+
+    # ============ GLOBAL CONSTANTS ============    ========================================================================================================================
+
+
+    # ============ DUNDER METHODS ============      ========================================================================================================================
+    
+    
+    # ============ PUBLIC METHODS ============      ========================================================================================================================
+    
+
+    pass
