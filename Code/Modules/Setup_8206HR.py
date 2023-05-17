6a125aaeb726a8db8ce24307dde3054c370b7b0a
ThresaKelly <tkelly@pinnaclet.com>
2023-05-17 13:39:52 -0500

    [PA-16] new file

---
+++ Code/Modules/Setup_8206HR.py
@@ -0,0 +1,19 @@
+"""
+Setup_8206HR provides the setup functions for an 8206-HR POD device.
+"""
+
+# enviornment imports
+
+# local imports
+from Setup_PodInterface import Setup_Interface
+
+# authorship
+__author__      = "Thresa Kelly"
+__maintainer__  = "Thresa Kelly"
+__credits__     = ["Thresa Kelly", "Seth Gabbert"]
+__license__     = "New BSD License"
+__copyright__   = "Copyright (c) 2023, Thresa Kelly"
+__email__       = "sales@pinnaclet.com"
+
+class Setup_8206HR(Setup_Interface) : 
+    pass
