241830fcc2f9e2ee893ee1e185525cd2aa7e2558
ThresaKelly <tkelly@pinnaclet.com>
2023-05-18 16:08:28 -0500

    [PA-16] create simple example

---
+++ Code/Examples/Using_SetupPod.py
@@ -0,0 +1,23 @@
+"""
+Simple example template that runs SetupPodDevices. 
+"""
+# set path to <path>\Python-POD-API\Code\Modules
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(sys.path[0]),'Modules')) 
+
+# local imports
+from SetupPodDevices import SetupPodDevices
+
+# authorship
+__author__      = "Thresa Kelly"
+__maintainer__  = "Thresa Kelly"
+__credits__     = ["Thresa Kelly", "Seth Gabbert"]
+__license__     = "New BSD License"
+__copyright__   = "Copyright (c) 2023, Thresa Kelly"
+__email__       = "sales@pinnaclet.com"
+
+# ===============================================================
+
+# setup 8206HR devices for streaming
+go = SetupPodDevices()
+go.Run()
