4953d6e408649fe1e5c4e5903d237fd28b888174
ThresaKelly <tkelly@pinnaclet.com>
2023-06-16 17:00:05 -0500

    [PA-32] format device 8206 HR docs

--- Code/Modules/PodDevice_8206HR.py
+++ Code/Modules/PodDevice_8206HR.py
@@ -18,9 +18,10 @@ class POD_8206HR(POD_Basics) :
     POD_8206HR handles communication using an 8206HR POD device. 
     
     Attributes:
-        __B4LENGTH (int): class-level integer representing the number of bytes for a Binary 4 packet.
-        __B4BINARYLENGTH (int): class-level integer representing the number of binary bytes for a Binary 4 packet.
-        _preampGain (int): instance-level integer (10 or 100) preamplifier gain.
+        __B4LENGTH (int): Class-level integer representing the number of bytes for a Binary 4 packet.
+        __B4BINARYLENGTH (int): Class-level integer representing the number of binary bytes for a \
+            Binary 4 packet.
+        _preampGain (int): Instance-level integer (10 or 100) preamplifier gain.
     """
 
     # ============ GLOBAL CONSTANTS ============    ========================================================================================================================
@@ -36,13 +37,14 @@ class POD_8206HR(POD_Basics) :
 
 
     def __init__(self, port: str|int, preampGain: int, baudrate:int=9600) -> None :
-        """Runs when an instance is constructed. It runs the parent's initialization. Then it updates the _commands to contain 
-        the appropriate commands for an 8206-HR POD device. 
+        """Runs when an instance is constructed. It runs the parent's initialization. Then it updates \
+            the _commands to contain the appropriate commands for an 8206-HR POD device. 
 
         Args:
             port (str | int): Serial port to be opened. Used when initializing the COM_io instance.
-            preampGain (int): _description_
-            baudrate (int, optional): Integer baud rate of the opened serial port. Used when initializing the COM_io instance. Defaults to 9600.
+            preampGain (int): Preamplifier gain. Must be 10 or 100.
+            baudrate (int, optional): Integer baud rate of the opened serial port. Used when initializing \
+                the COM_io instance. Defaults to 9600.
 
         Raises:
             Exception: Preamplifier gain must be 10 or 100.
@@ -85,14 +87,17 @@ class POD_8206HR(POD_Basics) :
         """Overwrites the parent's method. Separates the components of a binary4 packet into a dictionary.
 
         Args:
-            msg (bytes): Bytes string containing a complete binary4 Pod packet:  STX (1 byte) + command (4 bytes) + packet number (1 bytes) 
-                + TTL (1 byte) + ch0 (2 bytes) + ch1 (2 bytes) + ch2 (2 bytes) + checksum (2 bytes) + ETX (1 byte)
+            msg (bytes): Bytes string containing a complete binary4 Pod packet:  STX (1 byte)  \
+                + command (4 bytes) + packet number (1 bytes) + TTL (1 byte) + ch0 (2 bytes) \
+                + ch1 (2 bytes) + ch2 (2 bytes) + checksum (2 bytes) + ETX (1 byte).
 
         Raises:
-            Exception: (1) the packet does not have the minimum number of bytes, (2) does not begin with STX, or (3) does not end with ETX.
+            Exception: (1) the packet does not have the minimum number of bytes, (2) does not begin \
+                with STX, or (3) does not end with ETX.
 
         Returns:
-            dict[str,bytes]: A dictionary containing 'Command Number', 'Packet #', 'TTL', 'Ch0', 'Ch1', and 'Ch2' in bytes.
+            dict[str,bytes]: A dictionary containing 'Command Number', 'Packet #', 'TTL', 'Ch0', 'Ch1', \
+                and 'Ch2' in bytes.
         """
         # Binary 4 format = 
         #   STX (1 byte) + command (4 bytes) + packet number (1 bytes) + TTL (1 byte) 
@@ -124,15 +129,18 @@ class POD_8206HR(POD_Basics) :
 
 
     def TranslatePODpacket_Binary(self, msg: bytes) -> dict[str,int|float|dict[str,int]] : 
-        """Overwrites the parent's method. Unpacks the binary4 POD packet and converts the values of the ASCII-encoded bytes into 
-        integer values and the values of binary-encoded bytes into integers. Channel values are given in Volts.
+        """Overwrites the parent's method. Unpacks the binary4 POD packet and converts the values of the \
+        ASCII-encoded bytes into integer values and the values of binary-encoded bytes into integers. \
+        Channel values are given in Volts.
 
         Args:
-            msg (bytes): Bytes string containing a complete binary4 Pod packet:  STX (1 byte) + command (4 bytes) + packet number (1 bytes) 
-                + TTL (1 byte) + ch0 (2 bytes) + ch1 (2 bytes) + ch2 (2 bytes) + checksum (2 bytes) + ETX (1 byte)
+            msg (bytes): Bytes string containing a complete binary4 Pod packet:  STX (1 byte) \
+                + command (4 bytes) + packet number (1 bytes) + TTL (1 byte) + ch0 (2 bytes) \
+                + ch1 (2 bytes) + ch2 (2 bytes) + checksum (2 bytes) + ETX (1 byte).
 
         Returns:
-            dict[str,int|float|dict[str,int]]: A dictionary containing 'Command Number', 'Packet #', 'TTL', 'Ch0', 'Ch1', and 'Ch2' as numbers.
+            dict[str,int|float|dict[str,int]]: A dictionary containing 'Command Number', 'Packet #', \
+            'TTL', 'Ch0', 'Ch1', and 'Ch2' as numbers.
         """
         # unpack parts of POD packet into dict
         msgDict = POD_8206HR.UnpackPODpacket_Binary(msg)
@@ -150,8 +158,8 @@ class POD_8206HR(POD_Basics) :
 
 
     def TranslatePODpacket(self, msg: bytes) -> dict[str,int|dict[str,int]] : 
-        """Overwrites the parent's method. Determines if the packet is standard or binary, and translates accordingly.  
-        Adds a check for the 'GET TTL PORT' command.
+        """Overwrites the parent's method. Determines if the packet is standard or binary, and \
+        translates accordingly. Adds a check for the 'GET TTL PORT' command.
 
         Args:
             msg (bytes): Bytes string containing either a standard or binary packet.
@@ -219,7 +227,8 @@ class POD_8206HR(POD_Basics) :
 
 
     def _BinaryBytesToVoltage(self, value: bytes) -> float :
-        """Converts a binary bytes value read from POD device and converts it to the real voltage value at the preamplifier input.
+        """Converts a binary bytes value read from POD device and converts it to the real voltage value \
+        at the preamplifier input.
 
         Args:
             value (bytes): Bytes string containing voltage measurement.
@@ -241,11 +250,14 @@ class POD_8206HR(POD_Basics) :
 
 
     def _Read_Binary(self, prePacket: bytes, validateChecksum:bool=True) -> bytes :
-        """After receiving the prePacket, it reads the 8 bytes(TTL+channels) and then reads to ETX (checksum+ETX). 
+        """After receiving the prePacket, it reads the 8 bytes(TTL+channels) and then reads to ETX \
+        (checksum+ETX). 
 
         Args:
-            prePacket (bytes): Bytes string containing the beginning of a POD packet: STX (1 byte) + command number (4 bytes).
-            validateChecksum (bool, optional): Set to True to validate the checksum. Set to False to skip validation. Defaults to True.
+            prePacket (bytes): Bytes string containing the beginning of a POD packet: STX (1 byte) \
+                + command number (4 bytes).
+            validateChecksum (bool, optional): Set to True to validate the checksum. Set to False to \
+                skip validation. Defaults to True.
 
         Raises:
             Exception: Bad checksum for binary POD packet read.
