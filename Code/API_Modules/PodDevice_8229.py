efe4badcffc5560d73b36dff39131ca255d5aeec
ThresaKelly <tkelly@pinnaclet.com>
2023-06-22 11:15:39 -0500

    [PA-36] new class

---
+++ Code/API_Modules/PodDevice_8229.py
@@ -0,0 +1,14 @@
+# local imports 
+from BasicPodProtocol import POD_Basics
+
+# authorship
+__author__      = "Thresa Kelly"
+__maintainer__  = "Thresa Kelly"
+__credits__     = ["Thresa Kelly", "Seth Gabbert"]
+__license__     = "New BSD License"
+__copyright__   = "Copyright (c) 2023, Thresa Kelly"
+__email__       = "sales@pinnaclet.com"
+
+class POD_8229(POD_Basics) : 
+
+    pass
