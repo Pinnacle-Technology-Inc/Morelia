f5bb852f80027ea9c0dbb8c700501125bd53175c
ThresaKelly <tkelly@pinnaclet.com>
2023-07-24 11:52:10 -0500

    [PA-43] setup file

---
+++ Code/API_Modules/PodPacket.py
@@ -0,0 +1,35 @@
+# authorship
+__author__      = "Thresa Kelly"
+__maintainer__  = "Thresa Kelly"
+__credits__     = ["Thresa Kelly", "Seth Gabbert"]
+__license__     = "New BSD License"
+__copyright__   = "Copyright (c) 2023, Thresa Kelly"
+__email__       = "sales@pinnaclet.com"
+
+
+class Packet : 
+    pass
+
+
+class Packet_Standard(Packet) : 
+    pass
+
+
+# class Packet_BinaryStandard(Packet) : 
+#     pass
+
+
+# class Packet_Binary2(Packet) : 
+#     pass
+
+
+# class Packet_Binary3(Packet) : 
+#     pass
+
+
+# class Packet_Binary4(Packet) : 
+#     pass
+
+
+# class Packet_Binary5(Packet) : 
+#     pass
