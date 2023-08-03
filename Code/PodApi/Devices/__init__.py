712764108e24d9599b57811d0e059d47884a9f22
ThresaKelly <tkelly@pinnaclet.com>
2023-08-03 10:08:33 -0500

    [PA-47] add SerialPorts to Devices package

--- Code/PodApi/Devices/__init__.py
+++ Code/PodApi/Devices/__init__.py
@@ -3,3 +3,4 @@ from PodApi.Devices.PodDevice_8206HR    import Pod8206HR
 from PodApi.Devices.PodDevice_8401HR    import Pod8401HR
 from PodApi.Devices.PodDevice_8229      import Pod8229
 from PodApi.Devices.PodDevice_8480SC    import Pod8480SC
+from PodApi.Devices import SerialPorts
