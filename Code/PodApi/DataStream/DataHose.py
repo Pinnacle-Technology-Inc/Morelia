0adb30fbbc12073a0665b72335886832fe930404
ThresaKelly <tkelly@pinnaclet.com>
2023-08-14 15:12:24 -0500

    [PA-55] extra cleanup for bad data

--- Code/PodApi/DataStream/DataHose.py
+++ Code/PodApi/DataStream/DataHose.py
@@ -28,11 +28,12 @@ class Hose :
         if(sampleRate < 1) : 
             raise Exception('[!] The sample rate mus be greater than zero.')
         # set variables 
-        self.sampleRate  : int   = int(sampleRate)
-        self.deviceValve : Valve = Valve(podDevice, streamCmd, streamPldStart, streamPldStop)
-        self.data: list[Packet|None] = []
-        self.timestamps: list[float] = []
-        self.numDrops: int = 0
+        self.sampleRate : int   = int(sampleRate)
+        self.deviceValve: Valve = Valve(podDevice, streamCmd, streamPldStart, streamPldStop)
+        self.data       : list[Packet] = []
+        self.timestamps : list[float] = []
+        self.numDrops   : int = 0
+        self.corruptedPointsRemoved: int = 0
         
     def StartStream(self) : 
         # check for good connection 
@@ -58,7 +59,7 @@ class Hose :
         self.deviceValve.Open()
         while(True) : 
             # initialize
-            data = [None] * self.sampleRate
+            data: list[Packet|None] = [None] * self.sampleRate
             ti = (round(time.time(),9)) # initial time (sec)
             # read data for one second
             i: int = 0
@@ -77,19 +78,33 @@ class Hose :
                         i += 1 # update looping condition 
                 except : 
                     # corrupted data here, leave None in data[i]
-                    i += 1 # update looping condition                 
+                    i += 1 # update looping condition          
             currentTime = self._Drop(currentTime, ti, data)
 
-    def _Drop(self, currentTime: float, ti: float, data) : 
+    def _Drop(self, currentTime: float, ti: float, data: list[Packet|None]) : 
         # get times 
         nextTime = currentTime + (round(time.time(),9) - ti)
-        # update trackers before looping again
-        self.timestamps = np.linspace( # evenly spaced numbers over interval.
+        timestamps: list[float] = np.linspace( # evenly spaced numbers over interval.
             currentTime,    # start time
             nextTime,       # stop time
             self.sampleRate # number of items 
         ).tolist()
+        # clean out corrupted data
+        self._Filter(data,timestamps)
+        # update trackers before looping again
+        self.timestamps = timestamps
         self.data = data
         self.numDrops += 1
         # finish
         return nextTime
+    
+    def _Filter(self, data: list[Packet|None], timestamps: list[float]) : 
+        # remove all corrupted data from lists
+        while(None in data) : 
+            # find where None is in the data list
+            i = data.index(None)
+            # remove data and timestamp where there is corrupted data
+            data.pop(i)
+            timestamps.pop(i)
+            # update counter
+            self.corruptedPointsRemoved += 1
