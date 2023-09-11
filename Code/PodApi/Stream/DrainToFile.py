41e2cedcaefde8b1a8a611fd5556cbc87ea88e1e
ThresaKelly <tkelly@pinnaclet.com>
2023-09-11 15:48:48 -0500

    [PA-56] thread drain to file
    untested

--- Code/PodApi/Stream/DrainToFile.py
+++ Code/PodApi/Stream/DrainToFile.py
@@ -1,3 +1,7 @@
+# enviornment imports
+import time
+from threading import Thread
+
 # local imports
 from PodApi.Stream import Bucket
 from PodApi.Stream.Drain import DrainToFile, DrainToTXT, DrainToEDF
@@ -21,9 +25,42 @@ class DrainBucket :
         else : 
             raise Exception('[!] Extension '+str(ext)+' is not supported.')    
            
-    def StartDrainingToFile():
-        pass
-        # check if data is streaming or if data available
-        # deque a drop
-        # save drop to file
-        # loop until streaming is done 
+    def IsDataAvailable(self) : 
+        return ( self.drainToFile.dataBucket.GetVolumeOfDrops() <= 0 )
+    
+    def IsCollecting(self) : 
+        return ( self.drainToFile.dataBucket.isCollecting )
+           
+    def DrainBucketToFile(self, timeout_sec: float=10.0, sleep: float=0.25) -> Thread : 
+        t: Thread = Thread(target=self._ThreadedDrainBucketToFile, args=(timeout_sec,sleep))
+        t.start()
+        return t 
+           
+    def _ThreadedDrainBucketToFile(self, timeout_sec: float=10.0, sleep: float=0.25) :
+        timeoutTicker: float = 0
+        # open file to save streaming data to 
+        self.drainToFile.OpenFile()
+        # loop while the pod device is streaming 
+        while(self.drainToFile.dataBucket.isCollecting) : 
+            try: 
+                timeoutTicker = self._DrainDropToFile(timeoutTicker,timeout_sec,sleep)
+            except Exception as e: 
+                print(e)
+                break # force exit loop 
+        # finish 
+        self.drainToFile.CloseFile()
+    
+    def _DrainDropToFile(self, timeoutTicker: float, timeout_sec: float=10.0, sleep: float=0.25) -> float : 
+        # delay if there is no data to save
+        if(self.IsDataAvailable()) : 
+            # stop saving data if no drops
+            if(timeoutTicker == timeout_sec) : 
+                raise Exception('[!] Timeout ('+str(timeoutTicker)+'sec'+'): no data found to be saved to file.')
+            # wait for some time to let the bucket to collect
+            time.sleep(sleep)
+            return timeoutTicker+sleep
+        # otherwise, save data to file 
+        else:
+            self.drainToFile.DrainDropToFile()
+            return 0 # reset ticker 
+    
