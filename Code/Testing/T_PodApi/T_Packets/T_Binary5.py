e0993d259ccfdbb87621aa8e18e1e12c781536c2
ThresaKelly <tkelly@pinnaclet.com>
2024-01-04 14:29:59 -0600

    [PA-63] setup files

---
+++ Code/Testing/T_PodApi/T_Packets/T_Binary5.py
@@ -0,0 +1,10 @@
+# local imports
+from Testing.T_PodApi.TestProtocol import RunningTests, TestResult
+
+# authorship
+__author__      = "Thresa Kelly"
+__maintainer__  = "Thresa Kelly"
+__credits__     = ["Thresa Kelly", "Sree Kondi", "Seth Gabbert"]
+__license__     = "New BSD License"
+__copyright__   = "Copyright (c) 2023, Thresa Kelly"
+__email__       = "sales@pinnaclet.com"
---
+++ Code/Testing/T_PodApi/T_Packets/__init__.py
@@ -0,0 +1,5 @@
+from Testing.T_PodApi.T_Packets import T_Packet
+from Testing.T_PodApi.T_Packets import T_Standard
+from Testing.T_PodApi.T_Packets import T_Binary
+from Testing.T_PodApi.T_Packets import T_Binary4 
+from Testing.T_PodApi.T_Packets import T_Binary5 
