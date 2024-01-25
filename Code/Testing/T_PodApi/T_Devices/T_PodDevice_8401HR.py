c99a632eb6b1f8db7689256590b8fb8d7e1e105f
ThresaKelly <tkelly@pinnaclet.com>
2024-01-25 16:53:55 -0600

    [PA-65] basic tests for 8401hr

--- Code/Testing/T_PodApi/T_Devices/T_PodDevice_8401HR.py
+++ Code/Testing/T_PodApi/T_Devices/T_PodDevice_8401HR.py
@@ -38,6 +38,9 @@ class T_Pod8401HR :
         # collect all tests
         tests = {
             "1. Ping:\t\t"          : self.Ping,
+            "2. Type:\t\t"          : self.Type,
+            "3. Firmware:\t\t"      : self.FirmVer,
+            "4. Sample rate:\t\t"   : self.SampRat,
         }
         return RunningTests.RunTests(tests, 'Pod8401HR', printTests=printTests)
     # ---------------------------------------------------------------------------------------------------------
@@ -52,4 +55,47 @@ class T_Pod8401HR :
         elif(r.CommandNumber()  != 2    ) :         return TestResult(False, 'Packet has an incorrect command number.')
         elif(r.Payload()        != None ) :         return TestResult(False, 'Packet has incorrect payload.')
         # otherwise good
+        return TestResult(True)
+
+    def Type(self) -> TestResult : 
+        # call command
+        r = self.pod.WriteRead('TYPE')
+        # check 
+        if(  not isinstance(r,Packet)) :            return TestResult(False, 'Command did not return a packet')
+        elif(not isinstance(r,PacketStandard)) :    return TestResult(False, 'Command did not return a standard packet')
+        elif(r.CommandNumber()  != 8     ) :        return TestResult(False, 'Packet has an incorrect command number.')
+        elif(r.Payload()        != (0x31,) ) :      return TestResult(False, 'Packet has incorrect payload.')
+        # otherwise good
+        return TestResult(True)
+        
+    def FirmVer(self) -> TestResult : 
+        # call command
+        r = self.pod.WriteRead('FIRMWARE VERSION')
+        # check 
+        if(  not isinstance(r,Packet)) :            return TestResult(False, 'Command did not return a packet')
+        elif(not isinstance(r,PacketStandard)) :    return TestResult(False, 'Command did not return a standard packet')
+        elif(r.CommandNumber()  != 12     ) :       return TestResult(False, 'Packet has an incorrect command number.')
+        elif(not isinstance(r.Payload(), tuple) 
+             or len(r.Payload()) != 3 ) :           return TestResult(False, 'Packet has incorrect payload.')
+        # otherwise good
+        return TestResult(True)
+    
+    
+    def SampRat(self) -> TestResult : 
+        sampleRate: int = 2500
+        # set
+        r = self.pod.WriteRead('SET SAMPLE RATE', sampleRate)
+        # check 
+        if(  not isinstance(r,Packet)) :            return TestResult(False, 'Command did not return a packet')
+        elif(not isinstance(r,PacketStandard)) :    return TestResult(False, 'Command did not return a standard packet')
+        elif(r.CommandNumber()  != 101     ) :      return TestResult(False, 'Packet has an incorrect command number.')
+        elif(r.Payload()        != None ) :         return TestResult(False, 'Packet has incorrect payload.')
+        # get
+        r = self.pod.WriteRead('GET SAMPLE RATE')
+        # check 
+        if(  not isinstance(r,Packet)) :            return TestResult(False, 'Command did not return a packet')
+        elif(not isinstance(r,PacketStandard)) :    return TestResult(False, 'Command did not return a standard packet')
+        elif(r.CommandNumber() != 100     ) :       return TestResult(False, 'Packet has an incorrect command number.')
+        elif(r.Payload()       != (sampleRate,) ) : return TestResult(False, 'Packet has incorrect payload.')
+        # otherwise good 
         return TestResult(True)
