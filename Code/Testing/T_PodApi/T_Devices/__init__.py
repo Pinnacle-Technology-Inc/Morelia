3b6eff6fb435299d4919e4a1661d39fb3ec8f914
james hurd <jhurd@pinnaclet.com>
2024-06-03 13:41:01 -0500

    [PA-72] unit test for 8229.

--- Code/Testing/T_PodApi/T_Devices/__init__.py
+++ Code/Testing/T_PodApi/T_Devices/__init__.py
@@ -1,4 +1,5 @@
 from Testing.T_PodApi.T_Devices import T_PodDevice_8206HR
 from Testing.T_PodApi.T_Devices import T_PodDevice_8401HR
+from Testing.T_PodApi.T_Devices import T_PodDevice_8229
 from Testing.T_PodApi.T_Devices import T_SerialPorts
 from Testing.T_PodApi.T_Devices import TestAll
