075e0745f7f23e6de325650d1010e05cd323126f
Sree Kondi <skondi@pinnaclet.com>
2024-06-04 11:32:49 -0500

    [PA-73] added the test for 8480SC

--- Code/Testing/T_PodApi/T_Devices/__init__.py
+++ Code/Testing/T_PodApi/T_Devices/__init__.py
@@ -1,4 +1,5 @@
 from Testing.T_PodApi.T_Devices import T_PodDevice_8206HR
 from Testing.T_PodApi.T_Devices import T_PodDevice_8401HR
+from Testing.T_PodApi.T_Devices import T_PodDevice_8480SC
 from Testing.T_PodApi.T_Devices import T_SerialPorts
 from Testing.T_PodApi.T_Devices import TestAll
