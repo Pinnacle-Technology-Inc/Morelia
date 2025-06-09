45b991633fcc86f5df54cc26a202eb7120a714e4
James Hurd <jhurd@pinnaclet.com>
2025-06-08 23:47:19 -0500

    updated docs for packet.

--- src/Morelia/packet/data/data_packet.py
+++ src/Morelia/packet/data/data_packet.py
@@ -1,5 +1,8 @@
 from Morelia.packet import PodPacket
 
 class DataPacket(PodPacket):
+    """
+    The parent class of all data packets. Purely used for strengthening type safety.
+    """
     def __init__(self, raw_packet: bytes, min_length: int) -> None:
         super().__init__(raw_packet, min_length)
