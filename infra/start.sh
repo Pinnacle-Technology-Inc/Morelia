7e28d2c9264140beaa160a604f444950df0d6a61
Josselyn T. Bui <Josselyn.TBui@gmail.com>
2025-06-25 10:19:59 -0500

    created a wsl-setup file. fixed minor redundancy issues with start.sh

--- infra/start.sh
+++ infra/start.sh
@@ -46,11 +46,10 @@ sudo terraform apply -auto-approve
 echo "Grafana server started on http://localhost:3000 (default unless explicitly changed)"
 echo "Influx server started on http://localhost:8086 (default unless explicitly changed)"
 
-# Detect new /dev/ttyUSB device
+# Detect new /dev/ttyUSB device and run the wsl-setup script to bind the new /dev/ttyUSB devices
 chmod +x wsl-setup.sh
 before=($(ls /dev/ttyUSB* 2>/dev/null))
        ./wsl-setup.sh "${before[@]}"
 
-# Run the wsl-setup script to bind the new /dev/ttyUSB devices
-bash wsl-setup.sh
+
 
