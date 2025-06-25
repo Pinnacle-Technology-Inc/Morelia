d83a78f416db669db294fdcaee3aab51ce84ba2e
Josselyn T. Bui <Josselyn.TBui@gmail.com>
2025-06-25 10:15:06 -0500

    updated start.sh file

--- infra/start.sh
+++ infra/start.sh
@@ -37,7 +37,7 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin 
 sudo apt-get install terraform
 
 touch terraform.tfvars
-cp "default-values.txt" "terraform.tfvars" 
+cp "default-values.txt" "terraform.tfvars"
 
 sudo terraform init
 sudo terraform refresh
@@ -46,5 +46,11 @@ sudo terraform apply -auto-approve
 echo "Grafana server started on http://localhost:3000 (default unless explicitly changed)"
 echo "Influx server started on http://localhost:8086 (default unless explicitly changed)"
 
-# Run the live demo !!
-python3 live_demo.py
+# Detect new /dev/ttyUSB device
+chmod +x wsl-setup.sh
+before=($(ls /dev/ttyUSB* 2>/dev/null))
+       ./wsl-setup.sh "${before[@]}"
+
+# Run the wsl-setup script to bind the new /dev/ttyUSB devices
+bash wsl-setup.sh
+
