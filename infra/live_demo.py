91e3e3ff638f07d0b5abb6c74a7600502278b6a1
Josselyn T. Bui <Josselyn.TBui@gmail.com>
2025-06-25 10:13:57 -0500

    updated the live_demo.py file

--- infra/live_demo.py
+++ infra/live_demo.py
@@ -6,12 +6,17 @@ Welcome to the live demo! Below is a simple script that will allow you to stream
 from Morelia.Devices import Pod8206HR
 from Morelia.Stream.sink import InfluxSink
 from Morelia.Stream.data_flow import DataFlow
+import sys
 
-# Recieve the correct device ID from the user for the live demo
-id = str(input("What was your device id#? (/dev/ttyUSB<id#>): "))
+# Pass the array of devices from the wsl-setup.sh script
+devices = sys.argv[1:]
 
-# Connect to 8206HR devices on on /dev/ttyUSB0-2 and set the preamplifer gain to 10.
-pod_influxdb = Pod8206HR(f'/dev/ttyUSB{id}', 10)
+# Inital live demo set up
+print("Starting the live demo...")
+
+# Connect to 8206HR devices on /dev/ttyUSB0-2 and set the preamplifer gain to 10.
+print(f"Connecting to pod device on {devices[0]} and setting the preamp gain to 10")
+pod_influxdb = Pod8206HR(devices[0], 10)
 
 # Create InfluxDB Sink
 influx_sink = InfluxSink(pod_influxdb)
@@ -21,7 +26,8 @@ mapping = [(pod_influxdb, [influx_sink])]
 flowgraph = DataFlow(mapping)
 
 print("Start of data collection!")
-flowgraph.collect_for_seconds(3*60)
+flowgraph.collect_for_seconds(60)
 
 print("End of data collection!")
+
 
