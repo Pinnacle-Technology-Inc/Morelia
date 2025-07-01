95bc0da1ecf4171015e1956ba2683cf228428b14
Josselyn T. Bui <67287626+J3onS3o@users.noreply.github.com>
2025-07-01 15:17:27 -0500

    Update quest_sink.py

--- src/Morelia/Stream/sink/quest_sink.py
+++ src/Morelia/Stream/sink/quest_sink.py
@@ -4,7 +4,7 @@ __author__      = 'Josselyn Bui'
 __maintainer__  = 'Josselyn Bui'
 __credits__     = ['James Hurd', 'Sam Groth', 'Thresa Kelly', 'Seth Gabbert', 'Andrew Huang', 'Josselyn Bui']
 __license__     = 'New BSD License'
-__copyright__   = 'Copyright (c) 2024, Josselyn Bui'
+__copyright__   = 'Copyright (c) 2025, Josselyn Bui'
 __email__       = 'sales@pinnaclet.com'
 
 import socket
