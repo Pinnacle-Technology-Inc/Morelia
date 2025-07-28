8dd9100a7e0d370af0de321d7d8540900c4cd337
Andrew Huang <andrewhuang2019@gmail.com>
2025-07-28 13:34:30 -0500

    reverted accidental push

--- src/Morelia/Devices/BasicPodProtocol.py
+++ src/Morelia/Devices/BasicPodProtocol.py
@@ -388,8 +388,7 @@ class Pod :
                 cmd += b
             # start over if STX is found 
             if(b == PodPacket.STX ) : 
-                raise ValueError("Unexpected STX found while reading to ETX in cmd num.")
-                #self._read_pod_packet_recursive(validate_checksum=validate_checksum)
+                self._read_pod_packet_recursive(validate_checksum=validate_checksum)
             # return if ETX is found
             if(b == PodPacket.ETX ) : 
                 return(cmd)
